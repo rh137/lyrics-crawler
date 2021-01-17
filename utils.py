d0749c6c4e40e69b3a8a30b77dc9e620f3991589
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-18 03:56:02 +0800

    [utils] subtask 2, 3 basically done

---
+++ utils.py
@@ -0,0 +1,30 @@
+import requests as rq
+from bs4 import BeautifulSoup as bs
+
+def check(name1, name2):
+	return name1 in name2 or name2 in name1
+
+def kkbox_search(album_name='房間裡的大象', artist_name='持修'):
+	'''Given album name and artist name, return candidate album links'''
+	url =  'https://www.kkbox.com/tw/tc/search.php?'
+	params = {	'word': 	f'{album_name}+{artist_name}',
+				'search':	 'album'						}
+	r = rq.get(url, params=params)
+	s = bs(r.text, 'html.parser')
+	c = s.find_all('div', class_='height-wrap')
+	ret = []
+	for _ in c:
+		link = 'https://www.kkbox.com' + _.h3.a['href']
+		# returned album name
+		abn = _.h3.a.text.strip()
+		# returned artist name
+		atn = _.find('span', class_='name').text.strip()
+
+		if check(album_name, abn) and check(artist_name, atn):
+			ret.append((abn, atn, link))
+
+	return ret
+
+
+result = kkbox_search()
+print(result)
