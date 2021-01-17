34bcc4858cc56bd69280a517369756a27189f2b6
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-18 04:55:51 +0800

    [utils] rename variables

--- utils.py
+++ utils.py
@@ -10,24 +10,23 @@ KKBOX_DOMAIN_NAME = 'https://www.kkbox.com'
 def check(name1, name2):
 	return name1 in name2 or name2 in name1
 
-def kkbox_search(album_name='房間裡的大象', artist_name='持修'):
+def kkbox_search(searching_album_name='房間裡的大象', searching_artist_name='持修'):
 	'''Given album name and artist name, return candidate albums'''
 	url =  'https://www.kkbox.com/tw/tc/search.php?'
-	params = {	'word': 	f'{album_name}+{artist_name}',
+	params = {	'word': 	f'{searching_album_name}+{searching_artist_name}',
 				'search':	 'album'						}
 	r = rq.get(url, params=params)
 	s = bs(r.text, 'html.parser')
 	c = s.find_all('div', class_='height-wrap')
 	ret = []
 	for _ in c:
-		link = KKBOX_DOMAIN_NAME + _.h3.a['href']
-		# returned album name
-		abn = _.h3.a.text.strip()
-		# returned artist name
-		atn = _.find('span', class_='name').text.strip()
+		album_link = KKBOX_DOMAIN_NAME + _.h3.a['href']
+		album_name = _.h3.a.text.strip()
+		artist_name = _.find('span', class_='name').text.strip()
 
-		if check(album_name, abn) and check(artist_name, atn):
-			ret.append(Album(abn, atn, link))
+		if  check(album_name, searching_album_name)\
+			and check(artist_name, searching_artist_name):
+			ret.append(Album(album_name, artist_name, album_link))
 
 	return ret
 
@@ -37,7 +36,6 @@ def crawl_songs(album):
 	soup = bs(r.text, 'html.parser')
 	songs = soup.find_all('tr', class_='song-item')
 	for s in songs:
-#		song_data = s.find('td', class_='song-data')
 		song_title_a = s.find('a', class_='song-title')
 		song_name = song_title_a.text.strip()
 		song_link = KKBOX_DOMAIN_NAME + song_title_a['href']
