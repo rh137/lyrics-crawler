6a9a380cd9ef9b76d9ff8c50f5e585b918124bbd
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-23 01:29:20 +0800

    [song] add attributes "album", "artist" to the class "Song"

--- song.py
+++ song.py
@@ -1,8 +1,15 @@
 class Song:
-	def __init__(self, title, link):
+	def __init__(self, album, artist, title, link):
+		self._album = album
+		self._artist = artist
 		self._title = title
 		self.link = link
 		self.lyrics = ''
-
+	
+	def info(self):
+		return {'album':	self._album,
+				'artist':	self._artist,
+				'title':	self._title,}
+	
 	def __repr__(self):
 		return self._title
