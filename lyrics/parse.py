f6150e77b5aa39f7509b7b6cf500fff89c18960a
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-24 04:01:54 +0800

    [parse] clean lyrics data

---
+++ lyrics/parse.py
@@ -0,0 +1,19 @@
+import os
+
+if __name__ == '__main__':
+	file_names = os.listdir()
+	for name in file_names:
+		if '.py' in name or os.path.isdir(name): continue
+		with open(name, 'r') as f, open(f'./processed/{name}.txt', 'w') as fout:
+			for line in f:
+				if  '作詞：' in line or '作曲：' in line\
+					or '編曲：' in line or '詞/曲：' in line\
+					or '詞曲：' in line or '製作：' in line\
+					or '旁白：' in line or '編 曲：' in line\
+					or '詞、曲：' in line or '曲、詞：' in line\
+					or '詞：' in line or '曲：' in line\
+					or '編：' in line or '鼓吹：' in line\
+					or 'feat.' in line:
+					pass
+				else:
+					fout.write(line)
