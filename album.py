afcb2cf4b14f86d690f25384a1e352340211dc10
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-24 03:14:27 +0800

    [album] change member visibility; add getters for title and artist

--- album.py
+++ album.py
@@ -1,9 +1,15 @@
 class Album:
 	def __init__(self, title, artist, link):
-		self._title = title
-		self._artist = artist
+		self.__title = title
+		self.__artist = artist
 		self.link = link
 		self.songs = []
 	
+	def title(self):
+		return self.__title
+	
+	def artist(self):
+		return self.__artist
+
 	def __repr__(self):
-		return f'{self._title} - {self._artist}'
+		return f'{self.__title} - {self.__artist}'
