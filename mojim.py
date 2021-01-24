d0ff7b6454466a9856cd27055899146b7f80ad1e
rh137 <b06902137@csie.ntu.edu.tw>
2021-01-24 18:56:04 +0800

    [mojim] crawl songs from mojim.com done

--- mojim.py
+++ mojim.py
@@ -2,8 +2,8 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 import re
 
-import album
-import song
+import album_class
+import song_class
 
 from utils import Thesaurus
 
@@ -38,12 +38,32 @@ def mojim_search(searching_album_name='房間裡的大象', searching_artist_nam
 				continue
 			if album_name in searching_album_name or searching_album_name in album_name:
 				album_link = MOJIM_DOMAIN_NAME[:-1] + span.a['href']
-				ret.append(album.Album(album_name, artist_name, album_link))
+				ret.append(album_class.Album(album_name, artist_name, album_link))
 				FOUND = True
 				break
 	return ret
 
 
-if __name__ == '__main__':
-	mojim_search()
+def mojim_crawl_songs(album):
+	r = rq.get(album.link)
+	s = bs(r.text, 'html.parser')
+	c = s.find_all('span', class_='hc3')
+	for _ in c:
+		songs = _.find_all('a')
+		for song in songs:
+			if  '專輯歌曲' in song['title']\
+				or ('提供' in song['title'] and '謝謝您' in song['title']):
+				continue
+			else:
+				song_name = song.text.strip()
+				song_link = MOJIM_DOMAIN_NAME[:-1] + song['href']
+				print(song_name, song_link, flush=True)
 
+				album.songs.append(song_class.Song(album.title(), album.artist(), song_name, song_link))
+				
+
+
+if __name__ == '__main__':
+	ret = mojim_search('藍寶石', '濁水溪公社')
+	mojim_crawl_songs(ret[0])
+	print(ret[0].songs)
